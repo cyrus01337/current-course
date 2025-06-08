7fae0f95d46ac3d7849961b8ce677e0ae20ec650
cyrus01337 <cyrus01337@gmail.com>
2025-06-08 20:05:10 +0100

    Remove balls

--- src/app.jsx
+++ src/app.jsx
@@ -12,10 +12,8 @@ ROOT.render(
     <StrictMode>
         <main className="font-sans">
             <ol className="list-inside list-decimal">
-                <li>balls</li>
-                <li>balls #2</li>
-                <li>balls #3</li>
             </ol>
+
             <DateCounter />
         </main>
     </StrictMode>,
